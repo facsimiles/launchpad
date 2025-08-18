1056cae3df820f37c399152194d2fd10d8efef84
Michael Hudson-Doyle <michael.hudson@canonical.com>
2025-08-19 11:39:34 +1200

    add an index on distroarchseries (distroseries, underlying_architecturetag)

--- database/schema/patch-2211-44-0.sql
+++ database/schema/patch-2211-44-0.sql
@@ -6,4 +6,7 @@ SET client_min_messages=ERROR;
 ALTER TABLE distroarchseries
     ADD COLUMN underlying_architecturetag text;
 
+CREATE INDEX distroarchseries__distroseries__underlying_architecturetag__idx
+    ON distroarchseries (distroseries, underlying_architecturetag);
+
 INSERT INTO LaunchpadDatabaseRevision VALUES (2211, 44, 0);
