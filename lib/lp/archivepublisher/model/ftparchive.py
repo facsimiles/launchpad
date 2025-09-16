f1e77d075e8ae660750da37a3545d070e0489e94
Michael Hudson-Doyle <michael.hudson@canonical.com>
2025-09-17 11:05:01 +1200

    pass -oAPT::FTPArchive::FilterByFileNameArchitecture=false" when invoking apt-ftparchive

--- lib/lp/archivepublisher/model/ftparchive.py
+++ lib/lp/archivepublisher/model/ftparchive.py
@@ -179,7 +179,13 @@ class FTPArchiveHandler:
 
         stdout_handler = OutputLineHandler(self.log.debug, "a-f: ")
         stderr_handler = OutputLineHandler(self.log.info, "a-f: ")
-        base_command = ["apt-ftparchive"] + list(args) + [apt_config_filename]
+        base_command = (
+            [
+                "apt-ftparchive",
+            ]
+            + list(args)
+            + [apt_config_filename]
+        )
         spawner = CommandSpawner()
 
         returncodes = {}
@@ -207,7 +213,12 @@ class FTPArchiveHandler:
             )
 
     def runApt(self, apt_config_filename):
-        self.runAptWithArgs(apt_config_filename, "--no-contents", "generate")
+        self.runAptWithArgs(
+            apt_config_filename,
+            "-oAPT::FTPArchive::FilterByFileNameArchitecture=false",
+            "--no-contents",
+            "generate",
+        )
 
     #
     # Empty Pocket Requests
