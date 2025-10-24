e534da1710b51b641758576f04f5a57307d96508
Enrique Sanchez Cardoso <enrique.sanchezcardoso@canonical.com>
2025-10-24 17:58:01 +0200

    Fix import/export broken tests
    - Add `'` missing char
    - Add external link to result dict

--- lib/lp/bugs/browser/tests/test_buglisting.py
+++ lib/lp/bugs/browser/tests/test_buglisting.py
@@ -67,7 +67,7 @@ class TestBugTaskSearchListingPage(BrowserTestCase):
         # An external package from url will use unknown type
         self.assertTextMatchesExpressionIgnoreWhitespace(
             """
-            ep - Snap in Ep-distro
+            ep - Snap @12.1/stable in Ep-distro
             does not use Launchpad for bug tracking.
             Getting started with bug tracking in Launchpad.""",
             extract_text(top_portlet[0]),
@@ -195,7 +195,7 @@ class TestBugTaskSearchListingPage(BrowserTestCase):
         )
         self.assertTextMatchesExpressionIgnoreWhitespace(
             """
-            test-sp - Snap in Test-series does not
+            test-sp - Snap @12.1/stable in Test-series does not
             use Launchpad for bug tracking.
             Getting started with bug tracking in Launchpad.""",
             extract_text(top_portlet[0]),
