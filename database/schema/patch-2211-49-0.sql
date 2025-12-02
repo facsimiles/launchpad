c2961c1ac7d73a7fb4881d10515423d05473b29c
Enrique Sanchez Cardoso <enrique.sanchezcardoso@canonical.com>
2025-12-02 11:04:17 +0100

    Change publisher history tables naming
    - Add ArchivePublisherRun.status
    - Allow NULL ArchivePublisherRun.date_finished, then we can create the
      records at the start of a publisher run
    - Allow database UPDATE ArchivePublisherRun table

--- database/schema/patch-2211-49-0.sql
+++ database/schema/patch-2211-49-0.sql
@@ -3,36 +3,41 @@
 
 SET client_min_messages=ERROR;
 
-CREATE TABLE PublisherRun(
+CREATE TABLE ArchivePublisherRun(
     id serial PRIMARY KEY,
     date_started timestamp without time zone NOT NULL,
-    date_finished timestamp without time zone NOT NULL
+    date_finished timestamp without time zone,
+    status integer NOT NULL
 );
 
-CREATE INDEX publisherrun__date_started__idx ON PublisherRun
+CREATE INDEX archivepublisherrun__date_started__idx ON ArchivePublisherRun
   USING btree (date_started);
-CREATE INDEX publisherrun__date_finished__idx ON PublisherRun
+CREATE INDEX archivepublisherrun__date_finished__idx ON ArchivePublisherRun
   USING btree (date_finished);
+CREATE INDEX archivepublisherrun__status__idx ON ArchivePublisherRun
+ USING btree (status);
 
-COMMENT ON COLUMN PublisherRun.date_started IS
+COMMENT ON COLUMN ArchivePublisherRun.date_started IS
   'Start timestamp for the publisher run.';
-COMMENT ON COLUMN PublisherRun.date_finished IS
+COMMENT ON COLUMN ArchivePublisherRun.date_finished IS
   'End timestamp for the publisher run.';
+COMMENT ON COLUMN ArchivePublisherRun.status IS
+  'Status of the publisher run.';
 
-CREATE TABLE PublishingHistory(
+CREATE TABLE ArchivePublishingHistory(
     id serial PRIMARY KEY,
     archive integer NOT NULL REFERENCES archive,
-    publisher_run integer NOT NULL REFERENCES PublisherRun
+    publisher_run integer NOT NULL REFERENCES ArchivePublisherRun
 );
 
-CREATE INDEX publishinghistory__archive__idx ON PublishingHistory
+CREATE INDEX archivepublishinghistory__archive__idx ON ArchivePublishingHistory
   USING btree (archive);
-CREATE INDEX publishinghistory__publisher_run__idx ON PublishingHistory
+CREATE INDEX archivepublishinghistory__publisher_run__idx ON ArchivePublishingHistory
   USING btree (publisher_run);
 
-COMMENT ON COLUMN PublishingHistory.archive IS
+COMMENT ON COLUMN ArchivePublishingHistory.archive IS
   'Archive id that was published.';
-COMMENT ON COLUMN PublishingHistory.publisher_run IS
-  'Publisher run during which the archive was published.';
+COMMENT ON COLUMN ArchivePublishingHistory.publisher_run IS
+  'Archive publisher run during which the archive was published.';
 
 INSERT INTO LaunchpadDatabaseRevision VALUES (2211, 49, 0);
